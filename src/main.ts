2fa0e24521b93aaa1b590390245c81daba7b50b8
Tyler M. <J.t.moore4700@gmail.com>
2023-12-12 22:23:29 -0500

    fixed broken error checking

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { cardArray, suits, faces, IDeck } from "./deck";
 
-function deckGenerator(): IDeck[] {
+function deckGenerator(cardArray: number[], suits: string[], faces: string[] ): IDeck[] | Error {
     const newDeck: IDeck[] = [];
     for (let i = 0; i < suits.length; i++) { //this should never exceed 3.
         for (let c = 0; c < cardArray.length; c++) { //this should never exceed 13.
-            // if (cardArray[c] == undefined) {
-            //     return Error(`cardArray broken, verify contents`);
-            // }
-            // if (suits[i] == undefined) {
-            //     return Error(`suitsArray broken, verify contents`)
-            // }
-            // if (faces[c] == undefined) {
-            //     return Error(`suitsArray Broken, verify contents`)
-            // }
+            if (cardArray[c] == undefined) {
+                return Error(`cardArray broken, verify contents`);
+            }
+            if (suits[i] == undefined) {
+                return Error(`suitsArray broken, verify contents`)
+            }
+            if (faces[c] == undefined) {
+                return Error(`suitsArray Broken, verify contents`)
+            }
             const card = { points: cardArray[c], suit: suits[i], face: faces[c] }
             newDeck.push({card})
         }
@@ -21,4 +21,4 @@ function deckGenerator(): IDeck[] {
     return newDeck;
 }
 
-deckGenerator();
+deckGenerator(cardArray, suits, faces);
