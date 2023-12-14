603ad97972d1791a7df4adb5f2cd39592c89f7c6
Tyler M. <J.t.moore4700@gmail.com>
2023-12-14 18:14:59 -0500

    Improved Error checking, improved Readability by condensing error conditionals to 1-line statements. Added some fun. Implemented a functional shuffler

--- src/shuffler.ts
+++ src/shuffler.ts
@@ -5,17 +5,15 @@ const MAX_DECK_SIZE = 1000
 
 function shuffler(): ICard[] | Error {
     const orderedDeck = deckGenerator()
+    if (orderedDeck instanceof Error) return Error(`deckGenerator threw an error`)
     
     const shuffledDeck: ICard[] = []
-
-    if (orderedDeck instanceof Error) return Error(`deckGenerator threw an error`)
-
     // Checked the size to make sure it's safe to slice for performance.
     if (orderedDeck.length > MAX_DECK_SIZE) return Error(`Stop being stupid. MAX_DECK_SIZE less than deck length.`)
     const iterations = orderedDeck.length
     for (let i = 0; i < iterations; i++) {
         const cardIndex = getRandomIntInclusive(orderedDeck.length)
-        if (cardIndex instanceof Error) return Error(`getRandomIntInclusive `)
+        if (cardIndex instanceof Error) return Error(`getRandomIntInclusive failed. Somehow. Run away.`)
 
         const chosenCard = orderedDeck.splice(cardIndex - 1, 1)[0]
         if (chosenCard === undefined) return Error(`Why are multiple things accessing orderedDeck`)
